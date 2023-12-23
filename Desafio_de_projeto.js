7081ad035ee7ceaf0506f7b8f28a3ba4e31646d0
Eversoncooper <everson.coopeer@gmail.com>
2023-12-22 22:01:31 -0300

    Entrega do desafio de projeto

---
+++ Desafio_de_projeto.js
@@ -0,0 +1,25 @@
+var heroi_nome = "Homem-Aranha";
+var heroi_xp = 9001;
+
+if (heroi_xp < 1000){
+    console.log(heroi_nome + " está no nível ferro.")
+    }else if (heroi_xp >= 1001 && heroi_xp <= 2000){
+    console.log(heroi_nome + " está no nível bronze.")
+    }else if (heroi_xp >= 2001 && heroi_xp <= 5000){
+    console.log(heroi_nome + " está no nível prata.")
+    }else if (heroi_xp >= 5001 && heroi_xp <= 7000){
+    console.log(heroi_nome + " está no nível ouro.")
+    }else if (heroi_xp >= 7001 && heroi_xp <= 8000){
+    console.log(heroi_nome + " está no nível platina.")
+    }else if (heroi_xp >= 8001 && heroi_xp <= 9000){
+    console.log(heroi_nome + " está no nível ascendente.")
+    }else if (heroi_xp >= 9001 && heroi_xp <= 10000){
+    console.log(heroi_nome + " está no nível imortal.")
+    }else if (heroi_xp >= 10001){
+        console.log(heroi_nome + " está no nível radiante.")
+    }
+        
+    
+
+    
+
